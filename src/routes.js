35584011c4c4e1b25af4d784cc1b82486def55e3
GerinNugroho <gerinnugroho17@gmail.com>
2024-12-15 00:41:10 +0700

    fix bugs server

--- src/routes.js
+++ src/routes.js
@@ -18,12 +18,12 @@ const routes = [
   },
   {
     method: 'PUT',
-    path: 'notes/{id}',
+    path: '/notes/{id}',
     handler: editNoteByIdHandler,
   },
   {
     method: 'DELETE',
-    path: 'notes/{id}',
+    path: '/notes/{id}',
     handler: deleteNoteByIdHandler,
   },
 ];
